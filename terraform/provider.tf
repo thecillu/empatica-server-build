727cff50b643d5077805cdf8d98048009bd6a02c
Filippo Cilluffo <filippo.cilluffo@nexi.it>
2019-12-26 12:16:32 +0100

    removed profile

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,6 @@
 # Specify the provider and access details
 provider "aws" {
   shared_credentials_file = "$HOME/.aws/credentials"
-  profile                 = "empatica"
   region                  = var.aws_region
 }
 
